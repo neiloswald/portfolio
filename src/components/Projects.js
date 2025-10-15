f8a97c30a7c795c8cc37dbd53e7e622ac01fa798
Neil Bentulan <neil.bentulan@rev-kitten.com>
2025-10-15 20:06:30 +0800

    Add ArchiTwin and WiCare projects, update project details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,17 +11,32 @@ function Projects() {
         {
           id: 1,
           title: 'Virtual Traveler',
-          description: 'Tourists can experience immersive and interactive virtual tour to various destinations. This includes a tour admin dashboard, where one can create virtual tours, set points of interest, issue tickets for selling. Tourists buy tickets online and visits the tour. This utilizes Matterport to capture destinations and render digital twin of the space. A full-stack web application built with VueJS / Typescript and Python (AWS Lambda).',
-          technologies: ['VueJS', 'Typescript','Python', 'Mariadb', 'AWS Lambda', 'SocketIO', 'Matterport', 'Zoom Video API','Stripe Payment'],
+          description: 'Tourists can experience immersive and interactive virtual tour to various destinations. This includes a tour admin dashboard, where one can create virtual tours, set points of interest, issue tickets for selling. Tourists buy tickets online and visits the tour. This utilizes Matterport to capture destinations and render digital twin of the space. This also allows tour conductor and tourists to interact in a video meeting in space powered by Zoom Meeting API. A full-stack web application built with VueJS / Typescript and Python (AWS Lambda).',
+          technologies: ['VueJS', 'Typescript', 'Python', 'Oruga CSS', 'Mariadb', 'AWS Lambda','S3','SocketIO', 'Matterport', 'Zoom Video API', 'Stripe Payment'],
           link: '#'
         },
         {
           id: 2,
           title: 'AYA Remote School Life',
-          description: 'AYA is a remote school life system for young adults (ages between 15 and 39). This system specializes in avatar robot access from hospitals to schools. AYA application is a solution that will reduce the burden of the young adults from returning to real life society after being discharged from the hospital.',
-          technologies: ['VueJS', 'Typescript','Python', 'Mariadb','Matterport','AWS EC2','REST API'],
+          description: 'AYA is a remote school life system for adolescent and young adults. This system specializes in avatar robot access from hospitals to schools. AYA application is a solution that will reduce the burden of the young adults from returning to real life society after being discharged from the hospital.',
+          technologies: ['VueJS', 'Typescript', 'Python', 'Web2py', 'Oruga CSS', 'Mariadb', 'Matterport', 'AWS EC2', 'REST API'],
+          link: '#'
+        },
+        {
+          id: 3,
+          title: 'ArchiTwin (SaaS)',
+          description: 'ArchiTwin provides a navigable and interactive representation of the physical space, which can be explored in different perpectives, zoomed in and out, and even measure dimensions. Inside the virutal space we can add layers and virtual objects (which the users can experience and interact) like, tags, 3D models, video screens, slideshows and other custom objects. This project or framework is designed to be published as service for different user markets including, real estate, architecture and design, construction, tourism, remote collaboration. This is primarily power by Matterport technology.',
+          technologies: ['VueJS', 'Web2py', 'Python', 'Bulma CSS', 'Mariadb', 'Matterport', 'AWS EC2','S3', 'REST API'],
+          link: '#'
+        },
+        {
+          id: 4,
+          title: 'WiCare',
+          description: 'An app for monitoring elderly people in Elderly Shelters. Elderly is required to wear smartwatch which transmits vital signs like heartrate, gyro, temperature to  bluetooth data donggle installed in different locations in the house (giving elderly location). The donggle devices in turn transmits the collected data to the cloud server (REST API).',
+          technologies: ['VueJS', 'W', 'Python', 'Bulma CSS', 'Mariadb','AWS EC2', 'REST API','Smartwatch','Bluetooth Donggle'],
           link: '#'
         }
+
       ]
     },
     {
